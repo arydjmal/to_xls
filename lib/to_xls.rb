f357c391918130b268a8dd13c9aef6e410ee0630
Ary Djmal <arydjmal@gmail.com>
2009-01-11 01:10:02 -0500

    added a name to the worksheet; required.

--- lib/to_xls.rb
+++ lib/to_xls.rb
@@ -1,7 +1,7 @@
 class Array
   
   def to_xls(options = {})
-    output = '<?xml version="1.0" encoding="UTF-8"?><Workbook xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40" xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office"><Worksheet><Table>'
+    output = '<?xml version="1.0" encoding="UTF-8"?><Workbook xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40" xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office"><Worksheet ss:Name="Sheet1"><Table>'
     
     if self.any?
     
